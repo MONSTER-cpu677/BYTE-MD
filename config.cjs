c73855f8a37944732d2d82bb80c3f6b50d37cfd0
MONSTER-cpu677 <joshuandubueze13@gmail.com>
2025-05-02 18:29:37 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -1,4 +1,4 @@
-const fs = require('fs');
+I'mconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
@@ -7,8 +7,8 @@ module.exports = {
   "PREFIX": process.env.PREFIX || ".",
   "AUTO_READ_STATUS": process.env.AUTO_READ_STATUS === 'true',
   "MODE": process.env.MODE || "public",
-  "OWNER_NUMBER": process.env.OWNER_NUMBER || "923072380380",
-  "OWNER_NAME": process.env.OWNER_NAME || "Hamza",
+  "OWNER_NUMBER": process.env.OWNER_NUMBER || "09166427518,
+  "OWNER_NAME": process.env.OWNER_NAME || MONSTER TECH,
   "ALWAYS_ONLINE": process.env.ALWAYS_ONLINE === 'true',
   "PRESENCE": process.env.PRESENCE || "online",
   "OMDB_API_KEY": process.env.OMDB_API_KEY || "76cb7f39",
